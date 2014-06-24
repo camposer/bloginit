b3b86b3c7fa76b9ebaac092b48e777bee4cf4e3d
Rodolfo Campos <camposer@gmail.com>
2014-06-24 11:26:37 +0200

    POM actualizado con dependencias de JPA/Hibernate

---
+++ BlogInit/src/entity/Usuario.java
@@ -0,0 +1,55 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the USUARIO database table.
+ * 
+ */
+@Entity
+@Table(name="USUARIO")
+@NamedQuery(name="Usuario.findAll", query="SELECT u FROM Usuario u")
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(unique=true, nullable=false)
+	private Integer id;
+
+	@Column(length=20)
+	private String clave;
+
+	@Column(length=50)
+	private String nombre;
+
+	public Usuario() {
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getClave() {
+		return this.clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
